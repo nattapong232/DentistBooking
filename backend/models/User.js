17e4b6a1e4f272708453507c1a3db2bc93c4099f
Nattapong Anansomsin <wbook12514@gmail.com>
2024-04-18 22:09:49 +0700

    Modify User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,6 +7,11 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please add a name"],
   },
+  tel: {
+    type: String,
+    required: [true, "Please add a telephone number"],
+    match: [/^\+?[0-9()-\s]+$/],
+  },
   email: {
     type: String,
     required: [true, "Please add an email"],
